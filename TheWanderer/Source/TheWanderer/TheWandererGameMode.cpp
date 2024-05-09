d093c93f2667344aa27300c92a2474a098316500
Antonio Coronado <ant_cor94@hotmail.com>
2024-05-09 20:56:30 +0200

    Fixing Blueprint errors

--- TheWanderer/Source/TheWanderer/TheWandererGameMode.cpp
+++ TheWanderer/Source/TheWanderer/TheWandererGameMode.cpp
@@ -7,7 +7,7 @@
 ATheWandererGameMode::ATheWandererGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/_Game/Blueprints/Player/BP_TheWanderer"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
