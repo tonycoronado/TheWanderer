c534c93674975f042a87905850da314e531e562a
Antonio Coronado <ant_cor94@hotmail.com>
2024-05-19 20:30:52 +0200

    re-generating unreal files

--- TheWanderer/Source/TheWanderer/TheWanderer.Build.cs
+++ TheWanderer/Source/TheWanderer/TheWanderer.Build.cs
@@ -8,6 +8,6 @@ public class TheWanderer : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "GameplayAbilities", "GameplayTags", "GameplayTasks", "MotionWarping" });
 	}
 }
